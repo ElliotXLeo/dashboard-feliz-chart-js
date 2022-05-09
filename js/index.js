97c2feef018cad687a7f04470f2c764efb569baf
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-09 12:55:48 -0500

    html css js loader

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@
 import { fetchApi } from "../utils/utils.js";
 import features from "./features.js";
 import header from "./header.js";
+import loader from "./loader.js";
 import models from "./models.js";
 import years from "./years.js";
 
 const documentReady = async () => {
+  loader();
   header();
   const [allData, spainData] = await fetchApi('https://coasters-api.herokuapp.com', 'https://coasters-api.herokuapp.com/country/Spain');
   models(allData);
